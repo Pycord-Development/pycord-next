9c09a9efd67a1e42f47516e79d46694e88c6562f
VincentRPS <vincentbusiness55@gmail.com>
2022-07-12 16:59:57 +0800

    feat: add a spam channnel example

---
+++ examples-temp/spam_channel_creation.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from discord_typings import TextChannelData
+
+import pycord
+
+app = pycord.RESTApp()
+
+# please replace this
+GUILD: int = 0
+
+@app.listen('hook')
+async def on_hook():
+    ts = []
+
+    for _ in range(25):
+        ts.append(app.http.request(
+            'POST', f'/guilds/{GUILD}/channels', {'name': 'ratelimit-go-brr', 'type': 0}
+        ))
+
+    belts: list[TextChannelData] = await asyncio.gather(*ts)
+    ts.clear()
+
+    for belt in belts:
+        ts.append(app.http.request(
+            'DELETE', f'/channels/{belt["id"]}' 
+        ))
+
+    await asyncio.gather(*ts)
+
+app.run('token')
