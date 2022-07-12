0bc2b3de698ceaf52f6dd0b2ddc233986c647b26
VincentRPS <vincentbusiness55@gmail.com>
2022-07-12 17:25:27 +0800

    chore: lint example

--- examples-temp/spam_channel_creation.py
+++ examples-temp/spam_channel_creation.py
@@ -9,23 +9,21 @@ app = pycord.RESTApp()
 # please replace this
 GUILD: int = 0
 
+
 @app.listen('hook')
 async def on_hook():
     ts = []
 
     for _ in range(25):
-        ts.append(app.http.request(
-            'POST', f'/guilds/{GUILD}/channels', {'name': 'ratelimit-go-brr', 'type': 0}
-        ))
+        ts.append(app.http.request('POST', f'/guilds/{GUILD}/channels', {'name': 'ratelimit-go-brr', 'type': 0}))
 
     belts: list[TextChannelData] = await asyncio.gather(*ts)
     ts.clear()
 
     for belt in belts:
-        ts.append(app.http.request(
-            'DELETE', f'/channels/{belt["id"]}' 
-        ))
+        ts.append(app.http.request('DELETE', f'/channels/{belt["id"]}'))
 
     await asyncio.gather(*ts)
+
 
 app.run('token')
