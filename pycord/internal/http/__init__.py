f1128a951b64acbd31d2e90fb59070b85bb6c839
plun1331 <plun1331@gmail.com>
2022-07-14 20:45:42 -0700

    fix: inherit from EmojiRoutes

--- pycord/internal/http/__init__.py
+++ pycord/internal/http/__init__.py
@@ -16,13 +16,15 @@ from discord_typings.resources.user import UserData
 from pycord import __version__, utils
 from pycord.errors import Forbidden, HTTPException, NotFound, Unauthorized
 from pycord.internal.blocks import Block
-from pycord.internal.http.guild import GuildRoutes
 from pycord.internal.http.route import Route
+
+from pycord.internal.http.emoji import EmojiRoutes
+from pycord.internal.http.guild import GuildRoutes
 
 _log: logging.Logger = logging.getLogger(__name__)
 
 
-class HTTPClient(GuildRoutes):
+class HTTPClient(EmojiRoutes, GuildRoutes):
     def __init__(self, token: str, version: int, max_retries: int = 5):
         # pyright hates identifying this as clientsession when its not-
         # sadly, aiohttp errors a lot when not creating client sessions on an async environment.
