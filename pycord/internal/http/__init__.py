b66ccf9f57df365fcf29e9fbf2c49729da3734a7
Middledot <78228142+Middledot@users.noreply.github.com>
2022-07-17 17:42:41 -0400

    feat: file uploads (#21)
    * Implement File Uploads
    
    * Add some comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
    
    Signed-off-by: plun1331 <49261529+plun1331@users.noreply.github.com>
    
    * Apply code suggestions
    
    Co-Authored-By: plun1331 <49261529+plun1331@users.noreply.github.com>
    Co-Authored-By: Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
    
    * Remove _prepare_form typehints
    
    * Cleanup from testing
    
    * Update pycord/mixins.py
    
    Co-authored-by: Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
    
    Signed-off-by: plun1331 <49261529+plun1331@users.noreply.github.com>
    
    * Remove test.py from .gitignore
    
    * Update __init__.py
    
    Signed-off-by: Vincent <vincentbusiness55@gmail.com>
    
    * refactor: spoiler check
    
    Co-Authored-By: Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
    
    * Remove extra import
    
    Co-Authored-By: baronkobama <baronkobama@pycord.dev>
    
    Co-authored-by: plun1331 <49261529+plun1331@users.noreply.github.com>
    Co-authored-by: Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
    Co-authored-by: Vincent <vincentbusiness55@gmail.com>
    Co-authored-by: baronkobama <baronkobama@pycord.dev>

--- pycord/internal/http/__init__.py
+++ pycord/internal/http/__init__.py
@@ -10,10 +10,11 @@ Pycord's Internal HTTP Routes.
 import logging
 from typing import Any
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, FormData
 from discord_typings.resources.user import UserData
 
 from pycord import __version__, utils
+from pycord.file import File
 from pycord.errors import Forbidden, HTTPException, NotFound, Unauthorized
 from pycord.internal.blocks import Block
 from pycord.internal.http.emoji import EmojiRoutes
@@ -31,7 +32,6 @@ class HTTPClient(EmojiRoutes, GuildRoutes):
         self._headers: dict[str, str] = {
             'Authorization': 'Bot ' + token,
             'User-Agent': f'DiscordBot (https://github.com/pycord/pycord-v3, {__version__})',
-            'Content-Type': 'application/json',
         }
         self.version = version
         self._blockers: dict[str, Block] = {}
@@ -43,7 +43,14 @@ class HTTPClient(EmojiRoutes, GuildRoutes):
         self._session = ClientSession()
 
     async def request(
-        self, method: str, route: Route, data: dict[str, Any] | None = None, reason: str = None, **kwargs: Any
+        self,
+        method: str,
+        route: Route,
+        data: dict[str, Any] | None = None,
+        *,
+        files: list[File] | None = None,
+        reason: str = None,
+        **kwargs: Any
     ) -> dict[str, Any] | list[dict[str, Any]] | str | None:  # type: ignore
         endpoint = route.merge(self.url)
 
@@ -54,71 +61,116 @@ class HTTPClient(EmojiRoutes, GuildRoutes):
         if reason:
             headers['X-Audit-Log-Reason'] = reason
 
-        for _ in range(self.max_retries):
-            for blocker in self._blockers.values():
-                if (
-                    blocker.route.channel_id == route.channel_id
-                    or blocker.route.guild_id == route.guild_id
-                    or blocker.route.webhook_id == route.webhook_id
-                    or blocker.route.webhook_token == route.webhook_token
-                ):
-                    _log.debug(f'Blocking request to bucket {blocker.bucket_denom} prematurely.')
-                    await blocker.wait()
-                elif blocker.route.path == endpoint:
-                    _log.debug(f'Blocking request to bucket {blocker.bucket_denom} prematurely.')
-                    await blocker.wait()
-                    break
-                elif blocker.is_global:
-                    _log.debug(f'Blocking request to {endpoint} due to global ratelimit.')
-                    await blocker.wait()
-                    break
+        if files:
+            data = self._prepare_form(files, data)
+        elif data:
+            data = utils.dumps(data)
+            headers.update({"Content-Type": "application/json"})
 
-            r = await self._session.request(
-                method=method,
-                url=endpoint,
-                data=data if data is None else utils.dumps(data),
-                headers=headers,
-                **kwargs,
+        try:
+            for retry in range(self.max_retries):
+                if files:
+                    for f in files:
+                        f.reset(retry)
+
+                for blocker in self._blockers.values():
+                    if (
+                        blocker.route.channel_id == route.channel_id
+                        or blocker.route.guild_id == route.guild_id
+                        or blocker.route.webhook_id == route.webhook_id
+                        or blocker.route.webhook_token == route.webhook_token
+                    ):
+                        _log.debug(f'Blocking request to bucket {blocker.bucket_denom} prematurely.')
+                        await blocker.wait()
+                    elif blocker.route.path == endpoint:
+                        _log.debug(f'Blocking request to bucket {blocker.bucket_denom} prematurely.')
+                        await blocker.wait()
+                        break
+                    elif blocker.is_global:
+                        _log.debug(f'Blocking request to {endpoint} due to global ratelimit.')
+                        await blocker.wait()
+                        break
+
+                r = await self._session.request(
+                    method=method,
+                    url=endpoint,
+                    data=data,
+                    headers=headers,
+                    **kwargs,
+                )
+
+                # ratelimited
+                if r.status == 429:
+                    try:
+                        bucket = r.headers['X-RateLimit-Bucket']
+                    except:
+                        continue
+                    # block request until ratelimit ends.
+                    block = self._blockers.get(bucket)
+                    if block:
+                        await block.wait()
+                        continue
+                    else:
+                        block = Block(route)
+                        self._blockers[bucket] = block
+
+                        _log.debug(f'Blocking request to bucket {endpoint} after resource ratelimit.')
+                        await block.block(
+                            reset_after=float(r.headers['X-RateLimit-Reset-After']),
+                            bucket=bucket,
+                            globalrt=r.headers['X-RateLimit-Scope'] == 'global',
+                        )
+
+                        del self._blockers[bucket]
+                        continue
+
+                # something went wrong
+                if r.status >= 400:
+                    if r.status == 401:
+                        raise Unauthorized
+                    elif r.status == 403:
+                        raise Forbidden
+                    elif r.status == 404:
+                        raise NotFound
+                    else:
+                        raise HTTPException
+
+                _log.debug(f'Received {await r.text()} from request to {endpoint}')
+
+                return await utils._text_or_json(r)
+        finally:
+            if files:
+                for f in files:
+                    f.close()
+
+    def _prepare_form(self, files: list[File], payload: dict[str, Any] = {}) -> FormData:
+        form = []
+        attachments = []
+
+        for index, file in enumerate(files):
+            attachments.append(
+                {
+                    "id": index,
+                    "filename": file.filename,
+                    "description": file.description
+                }
+            )
+            form.append(
+                {
+                    "name": f"files[{index}]",
+                    "value": file.fp,
+                    "filename": file.filename,
+                    "content_type": "application/octet-stream"
+                }
             )
 
-            # ratelimited
-            if r.status == 429:
-                try:
-                    bucket = r.headers['X-RateLimit-Bucket']
-                except:
-                    continue
-                # block request until ratelimit ends.
-                block = self._blockers.get(bucket)
-                if block:
-                    await block.wait()
-                    continue
-                else:
-                    block = Block(route)
-                    self._blockers[bucket] = block
+        payload["attachments"] = attachments
+        form.insert(0, {"name": "payload_json", "content_type": "application/json", "value": utils.dumps(payload)})
+        form_data = FormData(quote_fields=False)
+        for f in form:
+            form_data.add_field(**f)
 
-                    _log.debug(f'Blocking request to bucket {endpoint} after resource ratelimit.')
-                    await block.block(
-                        reset_after=float(r.headers['X-RateLimit-Reset-After']),
-                        bucket=bucket,
-                        globalrt=r.headers['X-RateLimit-Scope'] == 'global',
-                    )
-
-                    del self._blockers[bucket]
-                    continue
-
-            # something went wrong
-            if r.status >= 400:
-                if r.status == 401:
-                    raise Unauthorized
-                elif r.status == 403:
-                    raise Forbidden
-                elif r.status == 404:
-                    raise NotFound
-                else:
-                    raise HTTPException
-
-            _log.debug(f'Received {await r.text()} from request to {endpoint}')
-            return await utils._text_or_json(r)
+        return form_data
 
     # this should get moved to an asset-related http file
     async def get_cdn_asset(self, url: str) -> bytes:
