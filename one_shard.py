b4fbd0efb5bca5e7259c444121e28282d443540e
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-07-18 08:12:20 -0700

    remove: file I keep including

--- one_shard.py
+++
@@ -1,8 +0,0 @@
-import pycord
-import logging
-
-app = pycord.GatewayApp(0, level=logging.DEBUG)
-
-
-# Connects the bot with the token `token` to the Discord Gateway
-app.connect('OTI0MTgzMjExOTIyMzEzMjE2.Gr72FH.OzjNKT3RHCD4qdfVl2y8AcuYp8an7M_vb0M41o')
