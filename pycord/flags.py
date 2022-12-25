71ecaa791680ff58fd2101321ebf51a3d7873fb4
VincentRPS <vincentbusiness55@gmail.com>
2022-12-25 13:45:57 +0800

    refactor: remove flag-based changes

--- pycord/flags.py
+++ pycord/flags.py
@@ -22,7 +22,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Literal, Type, TypeVar
+from typing import Literal, TypeVar
 
 F = TypeVar('F', bound='Flags')
 FF = TypeVar('FF')
@@ -67,18 +67,21 @@ class Flags:
                 raise AttributeError('as_bit is not a flag')
 
             flag_value = getattr(self.__class__, name)
-            self._flag_overwrites.append((flag_value, value))
+            self._overwrite_flag(flag_value, value)
 
-    def _has_flag(self, flag: int) -> tuple[int, bool]:
+    def _has_flag(self, flag: int) -> bool:
         return next(
-            (overwrite for overwrite in self._flag_overwrites if overwrite[0] == flag),
+            (
+                overwrite[1]
+                for overwrite in self._flag_overwrites
+                if overwrite[0] == flag
+            ),
             False,
         )
 
     def _overwrite_flag(self, flag: int, value: bool) -> None:
-        f = self._has_flag(flag=flag)
-        if f[1]:
-            self._flag_overwrites.remove(f)
+        if self._has_flag(flag=flag):
+            self._flag_overwrites.remove((flag, value))
 
         self._flag_overwrites.append((flag, value))
 
