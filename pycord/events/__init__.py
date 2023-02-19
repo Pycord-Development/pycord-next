0478d302e3d244f464fee37d151c5f38f5825412
plun1331 <plun1331@gmail.com>
2023-02-18 22:39:20 -0800

    feat: import all events

--- pycord/events/__init__.py
+++ pycord/events/__init__.py
@@ -6,5 +6,7 @@ Basic Implementation of Discord Events
 :copyright: 2021-present Pycord Development
 :license: MIT
 """
+from .channels import *
 from .event_manager import *
 from .guilds import *
+from .other import *
